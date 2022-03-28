c2f94c3f2e6afa86eb9b55cef4ec6e4ee1f0f11f
Sebastian Hidalgo <sebastianh.ing@gmail.com>
2022-03-28 00:41:47 -0500

    Options-api-1.0

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,6 +2,24 @@
 // })
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5,6,7]
-    
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Aliquip nulla elit laboris cupidatat enim ut. Occaecat incididunt sunt fugiat occaecat sunt laboris veniam eu. Deserunt consequat cupidatat laboris quis velit occaecat dolore dolore incididunt. Fugiat tempor aute eiusmod sit excepteur fugiat minim aute cillum aliqua incididunt consequat pariatur. Nisi officia magna officia ex nulla occaecat ea sunt non sit cillum Lorem reprehenderit aliqua. Do nulla fugiat et sunt do sit excepteur irure minim do et excepteur cupidatat sunt. Lorem duis sint cillum cillum in nulla fugiat ipsum duis duis laborum incididunt elit aliquip.',
+            picture: null, 
+        },
+        {
+            id: new Date().getTime() + 10000,
+            date: new Date().toDateString(),
+            text: 'Occaecat non aute cillum reprehenderit esse id officia minim ex ea magna cupidatat. Dolore sint aliqua anim id eu. Excepteur voluptate enim sunt nostrud reprehenderit. Tempor nulla ad consequat sit adipisicing officia sint est. Id nostrud tempor sit laboris sint adipisicing sunt aliquip labore deserunt ut ullamco dolore ullamco. Nulla duis ut dolor tempor labore nisi irure eu amet enim veniam nisi. Nostrud quis dolore deserunt dolor reprehenderit consectetur aliquip minim ex duis mollit culpa ut.',
+            picture: null, 
+        },
+        {
+            id: new Date().getTime() + 20000,
+            date: new Date().toDateString(),
+            text: 'Occaecat dolore qui fugiat pariatur commodo culpa cillum minim veniam pariatur dolore. Aliquip mollit qui reprehenderit enim veniam duis voluptate anim aliquip ea nulla anim. Incididunt eiusmod fugiat ut aute magna incididunt cillum mollit veniam cillum. Aliquip consectetur nisi fugiat occaecat ut dolore tempor laboris dolore. Qui ut exercitation sit mollit consequat nulla culpa ad pariatur fugiat.',
+            picture: null, 
+        },
+    ]
 })
